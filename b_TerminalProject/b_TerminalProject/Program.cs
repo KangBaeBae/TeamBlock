2c1789c3b5fc4e6587f28cc7fa7163115687e75c
KangBaeBae <qpqp9417@naver.com>
2017-05-27 12:02:53 +0900

    BinaryConvent DB

--- b_TerminalProject/b_TerminalProject/Program.cs
+++ b_TerminalProject/b_TerminalProject/Program.cs
@@ -847,13 +847,13 @@ namespace b_TerminalProject
                     {
                         db.Add(new List<string>());
                         db[i + 1].Add(category);
-                        db[i + 1].Add(val);
+                        db[i + 1].Add(ToBinary(val));
                         break;
                     }
 
                     else if (db[i][0] == category)
                     {
-                        db[i].Add(val);
+                        db[i].Add(ToBinary(val));
                     }
 				}
             }
@@ -862,16 +862,15 @@ namespace b_TerminalProject
             {
                 db.Add(new List<string>());
                 db[0].Add(category);
-                db[0].Add(val);
+                db[0].Add(ToBinary(val));
             }
         }
 
         public void ShowCategory()
         {
-            Console.WriteLine("hlhlhl");
             for (int i = 0; i < db.Count; i++)
             {
-                Console.Write("\t1 Category\t:\t" + db[i][0]);
+                Console.Write("\t" + (i + 1) + " Category\t:\t" + db[i][0]);
                 for (int j = 1; j < db[i].Count; j++)
                 {
                     Console.WriteLine();
@@ -880,6 +879,73 @@ namespace b_TerminalProject
                 Console.WriteLine();
             }
         }
+
+        string ToBinary(string data)
+        {
+            byte[] binary_byte = Encoding.Default.GetBytes(data);
+            string binary_string = string.Empty;
+
+            for (int i = 0; i < binary_byte.Length; i++)
+                binary_string += binary_byte[i];
+
+            return binary_string;
+        }
+
+        void Serialize(string path, Object obj)
+        {
+            Stream stream = File.Open(path, FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, obj);
+
+            stream.Close();
+        }
+
+        List<List<string>> DeSerialize(string path)
+        {
+            Stream st = File.Open(path, FileMode.Open);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            List<List<string>> mem = (List<List<string>>)bf.Deserialize(st);
+
+            st.Close();
+
+            return mem;
+        }
+
+        public void Save()
+        {
+            Console.Write("Enter the path : ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                Serialize(path, db);
+            }
+
+            catch
+            {
+                Console.WriteLine("Path Error");
+            }
+
+        }
+
+        public void Load()
+        {
+            Console.Write("Enter the path : ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                db = DeSerialize(path);
+            }
+
+            catch
+            {
+                Console.WriteLine("Path Error");
+            }
+        }
+
     }
 
 	class MainClass
